2867f7694b51ef19a45befbad82c65e030216c77
MichaelKla1 <100841670+MichaelKla1@users.noreply.github.com>
2024-08-02 14:26:43 +0300

    Add files via upload

---
+++ server for windows app/htdocs/config/connect.php
@@ -0,0 +1,31 @@
+﻿<?php
+if(!defined('DBHOST'))
+{
+    define( 'DBHOST', 'sql12.freemysqlhosting.net' );
+}
+if(!defined('DBUSER'))
+{
+    define( 'DBUSER', 'sql12722619' );
+}
+if(!defined('DBPASS'))
+{
+    define( 'DBPASS', 'WQ7LPFjd7d' );
+}
+if(!defined('DBNAME'))
+{
+    define( 'DBNAME', 'sql12722619' );
+}
+
+
+$dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . "";
+
+
+
+try {
+    $mysql = new PDO($dsn, DBUSER, DBPASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} catch (PDOException $e) {
+    die('Cant connect: ' . $e->getMessage());
+}
+
+
+?>
